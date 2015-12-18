3c3e155a9c075137f2a71d5191ac72cc099f30b8
johnko <git@johnko.ca>
2015-12-18 04:04:56 +0000

    don't ignore packages incase we lose them, allow optional host in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-DBNAME=wtfc
-DBURL=http://localhost:5984/${DBNAME}
+if [ "x" = "x$1" ]; then
+    HOST=localhost
+else
+    HOST=$1
+fi
+
+DBNAME=fileadmin
+DBURL=http://${HOST}:5984/${DBNAME}
 
 kanso install
 
